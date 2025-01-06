6ff5b4ded95ec38f763d5436e90864060a076d7f
hadje <hadjera.emroska@student.vinci.be>
2025-01-06 19:42:48 +0100

    8.5 good titles but without the authors

--- exercices/module8/ex1.js
+++ exercices/module8/ex1.js
@@ -97,7 +97,7 @@ const typeDefs = `
     dummy: Int
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -108,10 +108,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
+      let filteredBooks = books;
       if (args.author) {
-        return books.filter(book => book.author === args.author)
+        filteredBooks = filteredBooks.filter(book => book.author === args.author);
       }
-      return books
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter(book => book.genres.includes(args.genre));
+      }
+      return filteredBooks;
     },
     allAuthors: () => authors.map(author => ({
       name: author.name,
